cf23c2f89f5c23f45ffebc2469d4379d225b5a24
elii0017 <elii0017@student.monash.edu>
2024-10-06 12:34:04 +1100

    Update Week 10.js

--- docs/Week 10.js
+++ docs/Week 10.js
@@ -1,11 +1,11 @@
 // Embed the choropleth map
 var vg_1 = "./HomeworkWeek9.json";
-vegaEmbed("#choropleth_map", vg_1) {
+vegaEmbed("#choropleth_map", vg_1, { width: 800, height: 600 }).then(function(result) {
   // Access the Vega view instance if needed
 }).catch(console.error);
 
 // Embed the line chart
 var vg_2 = "./HomeworkWeek10.json";
-vegaEmbed("#line_chart", vg_2) {
+vegaEmbed("#line_chart", vg_2, { width: 800, height: 600 }).then(function(result) {
   // Access the Vega view instance if needed
 }).catch(console.error);
