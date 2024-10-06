d98a46b372293c7b3a5d31697a85f66cda31710e
elii0017 <elii0017@student.monash.edu>
2024-10-06 16:46:21 +1100

    Update Data-Visualisation-II.js

--- docs/Data-Visualisation-II.js
+++ docs/Data-Visualisation-II.js
@@ -5,24 +5,24 @@ vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 }).catch(console.error);
 
 // Embed the line chart for week 9
-var vg_2 = "/Visualisation Codes/proportion-indigenous.json"; // Adjust path
+var vg_2 = "./proportion-indigenous.json"; // Adjust path
 vegaEmbed("#pie-indigenous", vg_2).then(function(result) {
   // Access the Vega view instance if needed
 }).catch(console.error);
 
 // Embed the Pie Charts
-var vg_3 = "Visualisation Codes/proportion-non-indigenous.json"; // Adjust path
+var vg_3 = "./proportion-non-indigenous.json"; // Adjust path
 vegaEmbed("#pie_non_indigenous", vg_3).then(function(result) {
   // Access the Vega view instance if needed
 }).catch(console.error);
 
-var vg_4 = "Visualisation Codes/comparison.json"; // Adjust path
+var vg_4 = "./comparison.json"; // Adjust path
 vegaEmbed("#pie_comparison", vg_4).then(function(result) {
   // Access the Vega view instance if needed
 }).catch(console.error);
 
 // Embed the Stacked Bar Chart
-var vg_5 = "../Visualisation Codes/male.json"; // Adjust path
+var vg_5 = "./male-female.json"; // Adjust path
 vegaEmbed("#stacked_bar", vg_5).then(function(result) {
   // Access the Vega view instance if needed
 }).catch(console.error);
