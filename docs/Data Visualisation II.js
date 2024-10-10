24e34fe9f666cc6e6b9ba251079c706353afc10b
elii0017 <elii0017@student.monash.edu>
2024-10-10 15:29:44 +1100

    Update Data Visualisation II.js

--- docs/Data Visualisation II.js
+++ docs/Data Visualisation II.js
@@ -1,29 +1,28 @@
-// Embed the choropleth map
-var vg_1 = "./HomeworkWeek9.json"; // Adjust path to go up one directory
-vegaEmbed("#heat_map", vg_1).then(function(result) {
-  // Access the Vega view instance if needed
+var vg_1 = "./line-dot.json";
+vegaEmbed("sidebars",vg_1).then(function(results){
+}).catch(console.error);  
+
+var vg_2 = "./HomeworkWeek9.json"; 
+vegaEmbed("#heat_map", vg_2).then(function(result) {
 }).catch(console.error);
 
-// Embed the pie charts
-var vg_2 = "./slope-graphs.json"; // Adjust path
-vegaEmbed("#slopes", vg_2).then(function(result) {
-  // Access the Vega view instance if needed
+var vg_3 = "./HomeworkWeek10.json";
+vegaEmbed("#line_chart", vg_3).then(function(result) {
 }).catch(console.error);
 
-// Embed the Indigenous vs Non-Indigenous pie comparison
-var vg_3 = "./comparison.json"; // Adjust path
-vegaEmbed("#pie_comparison", vg_3).then(function(result) {
-  // Access the Vega view instance if needed
-}).catch(console.error);
-
-// Embed the Stacked Bar Chart
-var vg_4 = "./male-female.json"; // Adjust path
+var vg_4 = "./male-female.json"; 
 vegaEmbed("#stacked_bar", vg_4).then(function(result) {
-  // Access the Vega view instance if needed
 }).catch(console.error);
 
-// Embed the line chart for week 10
-var vg_5 = "./HomeworkWeek10.json"; // Adjust path to go up one directory
-vegaEmbed("#line_chart", vg_5).then(function(result) {
-  // Access the Vega view instance if needed
+var vg_5 = "./slope-graphs.json"; 
+vegaEmbed("#slopes", vg_5).then(function(result) {
 }).catch(console.error);
+
+var vg_6 = "./comparison.json"; 
+vegaEmbed("#pie_comparison", vg_6).then(function(result) {
+}).catch(console.error);
+
+var vg_7 = "./sidebarcharts.json";
+vegaEmbed("sidebars",vg_7).then(function(results){
+}).catch(console.error);
+
