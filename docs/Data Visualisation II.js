19b7dc8c36d7ef29a60164fce145db332fa0bb3b
elii0017 <elii0017@student.monash.edu>
2024-10-10 16:00:11 +1100

    Update Data Visualisation II.js

--- docs/Data Visualisation II.js
+++ docs/Data Visualisation II.js
@@ -10,7 +10,7 @@ var vg_3 = "./HomeworkWeek10.json";
 vegaEmbed("#line_chart", vg_3).then(function(result) {
 }).catch(console.error);
 
-var vg_4 = "./male-female.json"; 
+var vg_4 = "./male-female.json";
 vegaEmbed("#stacked_bar", vg_4).then(function(result) {
 }).catch(console.error);
 
@@ -22,7 +22,7 @@ var vg_6 = "./comparison.json";
 vegaEmbed("#pie_comparison", vg_6).then(function(result) {
 }).catch(console.error);
 
-var vg_7 = "./sidebarcharts.json";
+var vg_7 = "./side-barcharts.json";
 vegaEmbed("sidebars",vg_7).then(function(results){
 }).catch(console.error);
 
